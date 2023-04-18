9c069d6b163a988f2a7bfe0bf1567ad1d059cd53
MirruK <mirek.kudinoff@hotmail.com>
2023-04-19 00:32:28 +0300

    Support passing directories as arguments

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import sys
+import os
 from functools import reduce
 
 TOKENS: dict = {"TODO:" : 0}
-EOF = '\000'
+IGNORED: list[str] = [".git"]
+
+def recursive_get_files(dir: str, file_list: list[str]) -> None:
+    for ignored in IGNORED:
+        if(ignored in dir):
+            return
+    for file in os.listdir(dir):
+        file = os.path.join(dir, file)
+        if os.path.isdir(file):
+            recursive_get_files(file, file_list)
+        elif (os.path.isfile(file)): file_list.append(file)
 
 def fetch_files() -> list[str]:
     if(len(sys.argv) < 2):
-        raise ValueError("Usage: TOCO file(s)\nInvalid number of arguments")
+        raise ValueError("Brotha', you have to give me some files")
     input_files = []
     i = 1
     while(i < len(sys.argv)):
-        input_files.append(sys.argv[i])
+        if(os.path.isdir(sys.argv[i])): 
+            recursive_get_files(sys.argv[i], input_files) 
+        elif(os.path.isfile(sys.argv[i])): input_files.append(sys.argv[i])
+        else: print(f"Argument #{i}: Invalid file/directory supplied")
         i += 1
     assert len(input_files) > 0, "Something dun goofed"
     return input_files
@@ -56,6 +70,9 @@ def grep_todo(file_name: str) -> list[str]:
                 file_buffer.append(buffer.pop())
     return todos
 
+def compile_todos(todos: list[str]):
+    pass
+
 def main():
     """
     Flow of program:
@@ -64,12 +81,20 @@ def main():
         3. Collect said tokens
         4. Generate output from tokens
     """
+    # Step 1
     todo_files = fetch_files()
+    # Step 2
     todo_tokens = map(grep_todo, todo_files)
+    # Step 3
     todo_tokens = reduce(lambda x,y: x + y, todo_tokens)
     todo_tokens = filter(lambda x: x != '', todo_tokens)
+    # Step 4
+    # compile_todos(todo_tokens)
+    print("Found Files:")
     print(todo_files)
-    print(list(todo_tokens))
+    print("TODOs found:")
+    for token in list(todo_tokens):
+        print(token)
 
 
 main()
